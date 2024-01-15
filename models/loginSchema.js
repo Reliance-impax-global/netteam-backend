597e0c21d7d63c4d834af96696e3c1bdc266b61a
Harshit <harshitsingharya24@gmail.com>
2024-01-15 18:51:57 +0530

    updated Database

---
+++ models/loginSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const loginSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: {
+        type: String,
+    },
+    fancyId: String,
+    profilePic: { type: String, default: "" },
+    interests: { type: Array, default: [] },
+    tokens: { type: Number, default: 5 },
+    posts: { type: Array, default: [] },
+    saved: { type: Array, default: [] },
+    followers: { type: Array, default: [] },
+    following: { type: Array, default: [] },
+    socialId: { type: String, default: "" },
+    live: { type: Boolean, default: false },
+});
+
+const Login = mongoose.model("Login", loginSchema);
+module.exports = Login;
---
+++ models/messageSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+    from: String,
+    to: String,
+    message: String,
+    seen: { type: Boolean, default: false },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+module.exports=Message;
